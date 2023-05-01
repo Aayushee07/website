6b5a386582448bdf99cd9f847622c842d1b5571f
Aayushee07 <90083553+Aayushee07@users.noreply.github.com>
2023-05-01 09:40:20 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Aayushi Salunkhe",
+  title: "Web Designer & Enthusiast",
+  email: "aayushee2002@gmail.com",
+  gitHub: "https://github.com/Aayushee07",
+  linkedIn: "",
 };
 
 const primaryColor = "#4E567E";
